cf8b28e556b47a89b0cb2073f3ad5e7136a7dc7f
Fernando Baron <fernandobaron1@hotmail.com>
2019-09-07 17:27:17 -0300

    Modelo 1

---
+++ RaizQuadradaPotenciacao/src/raizquadradapotenciacao/RaizQuadradaPotenciacao.java
@@ -0,0 +1,19 @@
+
+package raizquadradapotenciacao;
+
+
+public class RaizQuadradaPotenciacao {
+
+   
+    public static void main(String[] args) {
+        
+           double base = 3;
+        double expoente = 3;
+        double resultado = Math.pow(base, expoente);
+        System.out.println(resultado);
+        
+        double raiz = Math.sqrt(4);
+        System.out.println(raiz);
+    }
+    
+}
